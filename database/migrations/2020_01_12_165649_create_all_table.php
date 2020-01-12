ce21c274ea9caaa117669fd5a4de6b63655fe1e0
truongan <truongan2005@gmail.com>
2020-01-13 02:16:07 +0700

    combine all table migration

---
+++ database/migrations/2020_01_12_165649_create_all_table.php
@@ -0,0 +1,143 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAllTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('username', 20);
+            $table->char('password', 100);
+            $table->char('display_name', 40);
+            $table->char('email', 40);
+            $table->char('role', 20);
+            $table->char('passchange_key', 60);
+            $table->dateTime('passchange_time');
+            $table->dateTime('first_login_time');
+            $table->dateTime('last_login_time');
+            $table->unsignedSmallInteger('selected_assigment');  
+            $table->timestamps();
+        });
+        Schema::create('languages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('name', 45);
+            $table->char('extension', 8);
+            $table->unsignedInteger('default_time_limit');
+            $table->unsignedInteger('default_memory_limit');
+            $table->unsignedInteger('sorting');  
+            $table->timestamps();
+        });
+        Schema::create('problems', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('name', 150);
+            $table->tinyInteger('is_upload_only');
+            $table->char('diff_cmd', 20);
+            $table->char('diff_arg', 20);
+            $table->text('admin_note');
+            $table->timestamps();
+        });
+        Schema::create('settings', function (Blueprint $table) {
+            $table->char('key', 50);
+            $table->text('value');
+            $table->timestamps();
+        });
+        Schema::create('queue', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('submit_id');
+            $table->char('username', 20);
+            $table->unsignedSmallInteger('assigment');  
+            $table->unsignedSmallInteger('problem'); 
+            $table->char('type', 8);
+            $table->unsignedInteger('process_id');
+            $table->timestamps();
+        });
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('name', 150);
+            $table->unsignedInteger('total_submits');
+            $table->tinyInteger('open');
+            $table->tinyInteger('score_board');
+            $table->tinyInteger('javaexceptions');
+            $table->dateTime('start_time');
+            $table->dateTime('finish_time');
+            $table->integer('extra_time');
+            $table->text('late_rule');
+            $table->text('participants');
+            $table->char('moss_update', 30);
+            $table->timestamps();
+        });
+        Schema::create('problem_assignment', function (Blueprint $table) {
+            $table->unsignedBigInteger('assignment_id');
+            $table->unsignedBigInteger('problem_id');
+            $table->integer('score');
+            $table->integer('ordering');
+            $table->char('problem_name', 150);
+            $table->timestamps();
+        });
+        Schema::create('submissions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('username',20);
+            $table->unsignedBigInteger('assignment_id');
+            $table->unsignedBigInteger('problem_id');
+            $table->tinyInteger('is_final');
+            $table->dateTime('time');
+            $table->char('status',100);
+            $table->integer('pre_score');
+            $table->char('coefficient',6);
+            $table->char('file_name',30);
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+        });
+        Schema::create('problem_language', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('language_id');
+            $table->unsignedBigInteger('problem_id');
+            $table->unsignedInteger('time_limit');
+            $table->unsignedInteger('memory_limit');
+            $table->timestamps();
+        });
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->char('title',200);
+            $table->text('text');
+            $table->dateTime('time');
+            $table->timestamps();
+        });
+        Schema::create('scoreboard', function (Blueprint $table) {
+            $table->unsignedSmallInteger('assigment'); 
+            $table->longText('scoreboard'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('languages');
+        Schema::dropIfExists('problems');
+        Schema::dropIfExists('settings');
+        Schema::dropIfExists('queue');
+        Schema::dropIfExists('assignments');
+        Schema::dropIfExists('problem_assignment');
+        Schema::dropIfExists('submissions');
+        Schema::dropIfExists('problem_language');
+        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('scoreboard');
+
+
+    }
+}
