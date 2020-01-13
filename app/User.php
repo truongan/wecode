e323473c279bdf99c63cdb729203efaaee3be58d
thanhnguyenfg <thanhnguyenfg@gmail.com>
2020-01-13 10:09:47 +0700

    User controller

--- app/User.php
+++ app/User.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+// use Illuminate\Database\Eloquent\Model;
+
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -37,3 +40,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
+
+//---------------------------------------------------------------------------------------------------------
