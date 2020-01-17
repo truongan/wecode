5661f00a5451666f308a48c30509a10ae96e011a
ry <conlocxoaythuytrieu@gmail.com>
2020-01-17 14:49:31 +0700

    add function at title_menu of views

--- resources/views/assignments.blade.php
+++ resources/views/assignments.blade.php
@@ -4,6 +4,11 @@
 
 @section('title', 'Assignments')
 
+@section('title_menu')
+    {{-- Nếu là admin thì hiển thị --}}
+    <span class="title_menu_item"><a href="{{ url('assignments/add') }}"><i class="fa fa-plus color8"></i> Add</a></span>
+@endsection
+
 @section('content')
 Nội dung ghi ở đây nè :3
 @endsection
--- resources/views/problems.blade.php
+++ resources/views/problems.blade.php
@@ -4,6 +4,16 @@
 
 @section('title', 'All Problems')
 
+@section('title_menu')
+    {{-- Nếu là admin thì hiển thị --}}
+    <span class="title_menu_item">
+        <a href="{{ url('problems/add') }}"><i class="fas fa-plus fa-lg color8"></i> Add</a>
+    </span>
+    <span class="title_menu_item">
+            <a href="{{ url('submissions/all/assignments/0') }}"><i class="fas fa-list-ul fa-lg color8"></i>Review test submissions for problems</a>
+    </span>
+@endsection
+
 @section('content')
 Nội dung ghi ở đây nè :3
 @endsection
