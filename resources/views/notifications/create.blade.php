2682437170cd593ad79690edb2204843871bbb8a
ry <conlocxoaythuytrieu@gmail.com>
2020-02-10 16:17:50 +0700

    build notification

--- resources/views/notifications/create.blade.php
+++ resources/views/notifications/create.blade.php
@@ -15,22 +15,18 @@ $(document).ready(function(){
 @endsection
 
 @section('content')
-{{-- notifications/({{$notif_edit}} ? 'edit/'+{{$notif_edit.id}} : 'add' ) --}}
-{!! Form::open(['method' => "POST", 'route' => 'notifications.store']) !!}
-{{-- @if ($notif_edit ?? '')
-	<input type="hidden" name="id" value="{{ $notif_edit.id }}"/>
-@endif --}}
-
-<p class="input_p">
-	<label for="form_title" class="tiny">Title:</label>
-	<input id="form_title" name="title" type="text" class="sharif_input" value=" $notif_edit.title "/>
-</p>
-<p class="input_p">
-	<label for="notif_text" class="tiny">Text:</label><br><br>
-	<textarea id="notif_text" name="text"> $notif_edit.text </textarea>
-</p>
-<p class="input_p">
-	<input type="submit" value=" $notif_edit ? 'Save' : 'Add' " class="sharif_input"/>
-</p>
-{!! Form::close() !!}
+<form method="POST"  action="{!! route('notifications.store') !!}">
+	<input type="hidden"  name ="_token" value="{!! csrf_token() !!}"/>
+	<p class="input_p">
+		<label for="form_title" class="tiny">Title:</label>
+		<input id="form_title" name="title" type="text" class="sharif_input"/>
+	</p>
+	<p class="input_p">
+		<label for="notif_text" class="tiny">Text:</label><br><br>
+		<textarea id="notif_text" name="text"></textarea>
+	</p>
+	<p class="input_p">
+		<input type="submit" value="Add" class="sharif_input"/>
+	</p>
+</form>	
 @endsection
