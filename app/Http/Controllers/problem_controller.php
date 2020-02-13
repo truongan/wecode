8fc9db446ce6dbe8c0f136cf0c65876d58bb5385
tks1998 <trankimsen1681998@gmail.com>
2020-02-13 12:49:37 +0700

    add problem

--- app/Http/Controllers/problem_controller.php
+++ app/Http/Controllers/problem_controller.php
@@ -68,9 +68,15 @@ class problem_controller extends Controller
      * @param  \App\Problem  $problem
      * @return \Illuminate\Http\Response
      */
-    public function edit(Problem $problem)
+    public function edit(Problem $id)
     {
-        //
+        if ( ! in_array( Auth::user()->role->name, ['admin', 'head_instructor', 'instructor']) )
+            abort(404);
+        if (!$request->has($request['file']))
+        {
+            return view('problems.list',['problems'=>Problem::all()]); 
+        }
+        return rediect('problems');
     }
 
     /**
@@ -91,17 +97,76 @@ class problem_controller extends Controller
      * @param  \App\Problem  $problem
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Problem $id)
-    {
-        if ( ! in_array( Auth::user()->role->name, ['admin', 'head_instructor', 'instructor']) )
-            abort(404);
-        if ($id!=NULL)
-            Problem::destroy($id);
-        return view('problems.list',['problems'=>Problem::all()]);  
-    }
+    // public function destroy(Problem $id)
+    // {
+    //     if ( ! in_array( Auth::user()->role->name, ['admin', 'head_instructor', 'instructor']) )
+    //         abort(404);
+    //     if ($id!=NULL)
+    //         Problem::destroy($id);
+    //     return view('problems.list',['problems'=>Problem::all()]);  
+    // }
 
-    public function add_problem()
+    // public function add()
+    // {
+    //     edit(NULL);
+    //     return view('problems.add');
+    // }
+
+    public function add_problem(Request $request)
     {
-        return view('problems.add');
+        // if (!$request->has($request['file']))
+        // {
+        //     return view('problems.list',['problems'=>Problem::all()]); 
+        // }
+        return view('languages.list');
     }
+    
+    // private function unload_zip_test_file($assignments_root, $problem_dir, $u_data){
+	// 	// Create a temp directory
+	// 	$tmp_dir_name = "shj_tmp_directory";
+	// 	$tmp_dir = "$assignments_root/$tmp_dir_name";
+	// 	shell_exec("rm -rf $tmp_dir; mkdir $tmp_dir;");
+
+	// 	// Extract new test cases and descriptions in temp directory
+	// 	$this->load->library('unzip');
+	// 	$this->unzip->allow(array('txt', 'cpp', 'html', 'md', 'pdf'));
+	// 	$extract_result = $this->unzip->extract($u_data['full_path'], $tmp_dir);
+
+	// 	// Remove the zip file
+	// 	unlink($u_data['full_path']);
+
+	// 	if ( $extract_result )
+	// 	{
+	// 		$this->clean_up_old_problem_dir($problem_dir);
+
+	// 		if (glob("$tmp_dir/*.pdf"))
+	// 			shell_exec("cd $problem_dir; rm -f *.pdf");
+	// 		// Copy new test cases from temp dir
+	// 		// echo $tmp_dir . "<br/>";
+	// 		// echo $problem_dir . "<br/>";
+	// 		// echo shell_exec("ls $tmp_dir/*");
+	// 		// echo "cp -R $tmp_dir/* $problem_dir;";
+	// 		//die();
+	// 		shell_exec("cp -R $tmp_dir/* $problem_dir;");
+	// 		$this->messages[] = array(
+	// 			'type' => 'success',
+	// 			'text' => 'Tests (zip file) extracted successfully.'
+	// 		);
+	// 	}
+	// 	else
+	// 	{
+	// 		$this->messages[] = array(
+	// 			'type' => 'error',
+	// 			'text' => 'Error: Error extracting zip archive.'
+	// 		);
+	// 		foreach($this->unzip->errors_array() as $msg)
+	// 			$this->messages[] = array(
+	// 				'type' => 'error',
+	// 				'text' => " Zip Extraction Error: ".$msg
+	// 			);
+	// 	}
+
+	// 	// Remove temp directory
+	// 	shell_exec("rm -rf $tmp_dir");
+	// }
 }
--- resources/views/problems/add.blade.php
+++ resources/views/problems/add.blade.php
@@ -1,11 +1,4 @@
-{{-- <fieldset class="form-group">
-    <div class="form-row">
-        <div class="col-sm-5">
-            <label for="diff_cmd">Diff command</label>
-        </div>
-        <div class="col-sm-7">
-            <input  type="text" name="diff_cmd" class="form-control col-xs-7" value="{{ edit_problem ? edit_problem.diff_cmd : set_value('diff_cmd', 'diff') }}"/>
-            {{ form_error('diff_cmd', '<div class="alert alert-danger">', '</div>') }}
-        </div>
-    </div>
-</fieldset> --}}
+<form action="{!! route('problem_controller.add_problem') !!}" enctype=“multipart/form-data” method=”post”>
+    <input type=“file” name=“zipFile” accept=“zip/*”>
+    <input type=“submit” name=“Submit” value=“Submit”>
+</form>
