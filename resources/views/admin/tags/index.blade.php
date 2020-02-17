cd9960e8c668b853be8e6ef31930052413f3c541
ry <conlocxoaythuytrieu@gmail.com>
2020-02-17 14:15:20 +0700

    table-responsive  ->  tags page

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -4,6 +4,10 @@
 
 @section('title','Tags')
 
+@section('other_assets')
+  <link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css'/>
+@endsection
+
 @section('title_menu')
 {{-- {% if user.level >= 2 %} --}}
 <div class="ml-4">
@@ -17,32 +21,39 @@
 @endsection
 
 @section('content')
-<div class="col">
-    <table class="wecode_table table table-striped table-bordered">
-    <thead class="thead-dark">
-    <tr>
-        <th>ID</th>
-        <th>Text</th>
-        <th>No. of Problems</th>
-        <th>Tools</th>
-    </tr>
-    </thead>
-    @foreach ($tags as $item)
-    <tr data-id="{{$item->id}}">
-        <td>{{ $item->id}}</td>
-        <td><a href="{{ route('tags.show', $item->id) }}">{{ $item->text }}</a></td>
-        <td>
-            {{$item->problems->count()}}
-        </td>
-        <td> 
-            <a title="Edit" href="{{ route('tags.edit', $item) }}"><i class="fas fa-edit fa-lg color9"></i></a>
-            <span title="Delete Tag" class="del_n delete_tag pointer"><i title="Delete Tag" class="far fa-trash-alt fa-lg color1"></i></span>
-        </td>
-    
-    </tr>
-    @endforeach
-    </table>
+<div class="row">
+    <div class="col">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>ID</th>
+                    <th>Text</th>
+                    <th>No. of Problems</th>
+                    <th>Tools</th>
+                </tr>
+                </thead>
+                @foreach ($tags as $item)
+                <tr data-id="{{$item->id}}">
+                    <td> {{$loop->iteration}} </td>
+                    <td>{{ $item->id}}</td>
+                    <td><a href="{{ route('tags.show', $item->id) }}">{{ $item->text }}</a></td>
+                    <td>
+                        {{$item->problems->count()}}
+                    </td>
+                    <td> 
+                        <a title="Edit" href="{{ route('tags.edit', $item) }}"><i class="fas fa-edit fa-lg color9"></i></a>
+                        <span title="Delete Tag" class="del_n delete_tag pointer"><i title="Delete Tag" class="far fa-trash-alt fa-lg color1"></i></span>
+                    </td>
+                
+                </tr>
+                @endforeach
+            </table>
+        </div>  
+    </div>
 </div>
+
 
 <div class="modal fade" id="tag_delete" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -63,6 +74,8 @@
 @endsection
 
 @section('body_end')
+<script type='text/javascript' src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type='text/javascript' src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
 <script>
 /**
 * Notifications
@@ -95,6 +108,10 @@ $('.del_n').click(function () {
   $("#tag_delete").modal("show");
 });
 
+    $("table").DataTable({
+		"pageLength": 10,
+		"lengthMenu": [ [10, 20, 30, 50, -1], [10, 20, 30, 50, "All"] ]
+	});
 });
 </script>
 @endsection
