ff2b5b1ddd9c51f3ee2da3b16044790386ebdee7
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2020-02-17 16:10:45 +0700

    revert change to dummy data

--- database/seeds/dummy_data.php
+++ database/seeds/dummy_data.php
@@ -17,80 +17,76 @@ class dummy_data extends Seeder
         // $this->call(UsersTableSeeder::class);
         // $this->call(installation_seeding::class);
 
-        // for ($i=3; $i < 10; $i++) { 
-        //     DB::table('users')->insert([
-        //         'username' => 'test' . $i,
-        //         'email' => 'test@def.com' . $i,
-        //         // 'email' => Str::random(10).'@gmail.com',
-        //         'password' => Hash::make('1234567890'),
-        //         'role_id' => $i % 4 + 1 //student
-        //     ]);
-        // }
-        // for ($i=1; $i < 10; $i++) { 
-        //     DB::table('problems')->insert([
-        //         'name' => 'problem' . $i,
-        //         'is_upload_only' => true,
-        //         'diff_cmd' => $i,
-        //         'diff_arg' => $i,
-        //         'admin_note' => 'fake_data'
-        //     ]);
-        // }
-        // for ($i=1; $i < 10; $i++) { 
-        //     DB::table('submissions')->insert([
-        //         'username' => 'username' . $i,
-        //         'assignment_id' => $i,
-        //         'problem_id'=>$i,
-        //         'is_final' => 1,
-        //         'time' => new DateTime,
-        //         'status' => $i,
-        //         'pre_score' => '100',
-        //         'coefficient'=>$i,
-        //         'file_name'=>$i,
-        //         'language_id'=>1
-        //     ]);
-        // }
-        // for ($i=1; $i < 10; $i++) { 
-        //     DB::table('assignments')->insert([
-        //         'name' => 'username' . $i,
-        //         'total_submits' => $i,
-        //         'open'=>$i,
-        //         'score_board' => $i,
-        //         'javaexceptions' => $i,
-        //         'start_time' => new DateTime,
-        //         'finish_time' => new DateTime,
-        //         'extra_time'=> $i,
-        //         'late_rule'=>$i,
-        //         'participants'=>$i,
-        //         'moss_update'=>$i,
-        //         'description'=>"....",
-        //         'created_at'=>new DateTime,
-        //         'updated_at'=>new DateTime
-        //     ]);
-        // }
-        // for ($i=1; $i < 8; $i++) { 
-        //     DB::table('language_problem')->insert([
-        //        'language_id' => $i,
-        //        'problem_id' =>$i,
-        //        'time_limit' =>100,
-        //        'memory_limit'=>100,
-        //        'created_at'=>new DateTime,
-        //        'updated_at'=>new DateTime
-        //     ]);
-        // }
-        for ($i=1; $i < 8; $i++) { 
+        for ($i=1; $i < 10; $i++) { 
+            DB::table('users')->insert([
+                'username' => 'test' . $i,
+                'email' => 'test@def.com' . $i,
+                // 'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('1234567890'),
+                'role_id' => $i % 4 + 1 //student
+            ]);
+        }
+        for ($i=1; $i < 10; $i++) { 
+            DB::table('tags')->insert([
+                'text' => 'tag' . $i
+            ]);
+        }
+        for ($i=1; $i < 10; $i++) { 
+            DB::table('problems')->insert([
+                'name' => 'problem' . $i,
+                'is_upload_only' => true,
+                'diff_cmd' => $i,
+                'diff_arg' => $i,
+                'admin_note' => 'fake_data'
+            ]);
+        }
+
+        $a = [1,2,3,4,5,6,7,8];
+        for($i = 1; $i < 5; $i++){
+            shuffle($a);
+            Tag::find($i)->problems()->attach(array_slice($a,0,rand()%5));
+        }
+
+        for ($i=1; $i < 10; $i++) { 
             DB::table('submissions')->insert([
-                "username"=>"test".$i,
-                "assignment_id"=>$i,
-                "problem_id"=>$i,
-                "is_final"=>True,
-                "time"=>new DateTime,
-                "status"=>"1",
-                "pre_score"=>"100",
-                "coefficient"=>"1",
-                "file_name"=>"1",
-                "language_id"=>"1",
-                "created_at"=>new DateTime,
-                "updated_at"=>new DateTime,
+                'username' => 'username' . $i,
+                'assignment_id' => $i,
+                'problem_id'=>$i,
+                'is_final' => 1,
+                'time' => new DateTime,
+                'status' => $i,
+                'pre_score' => '100',
+                'coefficient'=>$i,
+                'file_name'=>$i,
+                'language_id'=>1
+            ]);
+        }
+        for ($i=1; $i < 10; $i++) { 
+            DB::table('assignments')->insert([
+                'name' => 'username' . $i,
+                'total_submits' => $i,
+                'open'=>$i,
+                'description' => "",
+                'score_board' => $i,
+                'javaexceptions' => $i,
+                'start_time' => new DateTime,
+                'finish_time' => new DateTime,
+                'extra_time'=> $i,
+                'late_rule'=>$i,
+                'participants'=>$i,
+                'moss_update'=>$i,
+                'created_at'=>new DateTime,
+                'updated_at'=>new DateTime
+            ]);
+        }
+        for ($i=1; $i < 8; $i++) { 
+            DB::table('language_problem')->insert([
+               'language_id' => $i,
+               'problem_id' =>$i,
+               'time_limit' =>100,
+               'memory_limit'=>100,
+               'created_at'=>new DateTime,
+               'updated_at'=>new DateTime
             ]);
         }
         for ($i=1; $i < 8; $i++) { 
