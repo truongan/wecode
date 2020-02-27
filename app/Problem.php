432ea01806d0a6322e708927752aac4e2c2ed2c3
tks1998 <trankimsen1681998@gmail.com>
2020-02-27 10:33:53 +0700

    fix delete problem

--- app/Problem.php
+++ app/Problem.php
@@ -15,18 +15,6 @@ class Problem extends Model
         return $this->belongsToMany('App\Language')->withTimestamps()->withPivot('time_limit','memory_limit');
     }
 
-    public  function problem_info_detailed(){
-        
-        $query = Problem::find($id);
-        
-        $result['no_of_ass'] = $this->assigments->count();
-        $result['no_of_sub'] = $this->submissions->count();
-        
-        if($result != NULL) $result['languages'] = $this->all_languages;
-       
-        return $result;
-	}
-
     public function assignments()
     {
         return $this->belongsToMany('App\Assignment');
