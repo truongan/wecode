295c844006bb62182c69889166e9fab1ce652621
truongan <truongan2005@gmail.com>
2020-03-01 18:34:36 +0700

    working on submit_page.js

--- app/Http/Controllers/submission_controller.php
+++ app/Http/Controllers/submission_controller.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\DB;
 
 class submission_controller extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth'); // phải login
+    }
+
     /**
      * Display a listing of the resource.
      *
