c93fcc453169d959e1099d85954f729ff74c55d5
thanhnguyenfg <thanhnguyenfg@gmail.com>
2020-03-03 17:00:03 +0700

    Queue

--- app/Queue_item.php
+++ app/Queue_item.php
@@ -13,20 +13,4 @@ class Queue_item extends Model
     {
         return $this->hasOne('App\Submission');
     }
-
-    public function in_queue ($user_id, $assignment_id, $problem_id)
-	{
-		$queries = Queue_item::all();
-        foreach ($queries as $query)
-        {
-            $query->submission->where(array('user_id' => $user_id, 'assignment_id' => $assignment_id, 'problem_id' => $problem_id))->get();
-            if ($query->num_rows() > 0) return TRUE;
-        }
-		return FALSE;
-	}
-
-    public function add_to_queue($submit_info)
-    {
-        
-    }
 }
