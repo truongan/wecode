a4d6a66663cf8aa588863a712babf0f5b246557c
thanhnguyenfg <thanhnguyenfg@gmail.com>
2020-08-12 21:32:44 +0700

    Fix add language in create problem

--- app/Http/Controllers/problem_controller.php
+++ app/Http/Controllers/problem_controller.php
@@ -90,10 +90,14 @@ class problem_controller extends Controller
             }
         }
         
+        $langs = [];
+        foreach ($request->input('enable') as $i => $lang)
+            if ($lang == 1)
+                $langs += [$request->input('language_id')[$i] => ['time_limit' => $request->input('time_limit')[$i], 'memory_limit' => $request->input('memory_limit')[$i]]];
 
-        $default_language = Language::find(1);
-        
-        $the_id = $this->new_problem_id();
+        //$default_language = Language::find(1);
+
+        $the_id = Problem::max('id') + 1;
         $problem = $request->input();
         $problem['id'] = $the_id;
         $problem["allow_practice"] = isset($request["allow_practice"]) ? 1 : 0;
@@ -102,6 +106,8 @@ class problem_controller extends Controller
         {
             $p->tags()->sync($tags);
         }
+
+        $p->languages()->sync($langs);
 
         // Processing file 
         $this->_take_test_file_upload($request, $the_id, $messages);  
@@ -126,7 +132,7 @@ class problem_controller extends Controller
                                       'can_submit'=>TRUE,
                                       'assignment'=>NULL,
                                       ]);    
-	}
+    }
 
     /**
      * Show the form for editing the specified resource.
@@ -198,6 +204,7 @@ class problem_controller extends Controller
                     }
             }
         } 
+
         $problem->tags()->sync($tags);
 
         $this->replace_problem($request,$problem->id,$problem);
@@ -211,15 +218,15 @@ class problem_controller extends Controller
 
     public function _take_test_file_upload(Request $request, $the_id,  &$messages){
         $up_dir = $request->tests_dir;
-		$up_zip = $request->tests_zip;
+        $up_zip = $request->tests_zip;
         if (!$up_dir && !$up_zip){
             //             $messages = "Notice: You did not upload test case and description. If needed, upload by editing assignment.";
             return ;
-        }		
+        }       
         $assignments_root = Setting::get("assignments_root");
         $problem_dir = $this->get_directory_path($the_id);
      
-		if ( ! file_exists($problem_dir) ){
+        if ( ! file_exists($problem_dir) ){
                 mkdir($problem_dir, 0700, TRUE); 
         }
 
@@ -228,18 +235,18 @@ class problem_controller extends Controller
             
             shell_exec('rm -f '.$assignments_root.'/*.zip');
             
-			$name_zip = $request->tests_zip->getClientOriginalName();
+            $name_zip = $request->tests_zip->getClientOriginalName();
             $path_zip = $request->tests_zip->storeAs($assignments_root,$name_zip,'my_local');
     
-			$this->unload_zip_test_file($request , $assignments_root, $problem_dir, $messages, $name_zip);
+            $this->unload_zip_test_file($request , $assignments_root, $problem_dir, $messages, $name_zip);
 
-		} else {
+        } else {
             if ($up_dir)
             {
                 $this->handle_test_dir_upload($request,$assignments_root,$up_dir, $problem_dir, $messages);
             }
-		}
-	}
+        }
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -280,30 +287,30 @@ class problem_controller extends Controller
 
         }
         
-		header('Content-Type: application/json; charset=utf-8');  
+        header('Content-Type: application/json; charset=utf-8');  
         return ($json_result);
     }
 
     public function save_problem_description($problem_id, $text, $type = 'html')
-	{
-		$problem_dir = $this->get_directory_path($problem_id);
-		if (file_put_contents("$problem_dir/desc.html", $text) ) 
-			return true;
-		else return false;
+    {
+        $problem_dir = $this->get_directory_path($problem_id);
+        if (file_put_contents("$problem_dir/desc.html", $text) ) 
+            return true;
+        else return false;
     }
     
     public function edit_description(Request $request, $id){
-		if ( ! in_array( Auth::user()->role->name, ['admin', 'head_instructor', 'instructor']) )
+        if ( ! in_array( Auth::user()->role->name, ['admin', 'head_instructor', 'instructor']) )
             abort(404);
 
         // $request->validate([
         //     'content'=>['required','text']
         // ]); 
 
-		if ($this->save_problem_description($id, $request->content)){
+        if ($this->save_problem_description($id, $request->content)){
             echo "success";
                 return;
-		}
+        }
         else 
             echo "error";
     }
@@ -311,75 +318,75 @@ class problem_controller extends Controller
     private function unload_zip_test_file(Request $request, $assignments_root, $problem_dir, &$messages, $name_zip){
         
         // Create a temp directory
-		$tmp_dir_name = "shj_tmp_directory";
+        $tmp_dir_name = "shj_tmp_directory";
         $tmp_dir = "$assignments_root/$tmp_dir_name";
-		shell_exec("rm -rf $tmp_dir; mkdir $tmp_dir;");
+        shell_exec("rm -rf $tmp_dir; mkdir $tmp_dir;");
        
         // get new name
-		$rename_inputoutput = $request->rename_zip;
+        $rename_inputoutput = $request->rename_zip;
 
         // extract file 
         
         shell_exec("cd $assignments_root; unzip $name_zip -d $tmp_dir");
 
-		// Remove the zip file
-		shell_exec("cd $assignments_root; rm -rf $name_zip");
+        // Remove the zip file
+        shell_exec("cd $assignments_root; rm -rf $name_zip");
         
         $a=1;
         if ($a==1)
-		{
-			$this->clean_up_old_problem_dir($problem_dir);
+        {
+            $this->clean_up_old_problem_dir($problem_dir);
 
-			if (glob("$tmp_dir/*.pdf"))
-				shell_exec("cd $problem_dir; rm -f *.pdf");
+            if (glob("$tmp_dir/*.pdf"))
+                shell_exec("cd $problem_dir; rm -f *.pdf");
 
-			shell_exec("cp -R $tmp_dir/* $problem_dir;");
-			// rray(
-			// 	'type' => 'success',
-			// 	'text' => 'Tests (zip file) extracted successfully.'
-			// );
-			$in = glob("$problem_dir/in/*");
+            shell_exec("cp -R $tmp_dir/* $problem_dir;");
+            // rray(
+            //  'type' => 'success',
+            //  'text' => 'Tests (zip file) extracted successfully.'
+            // );
+            $in = glob("$problem_dir/in/*");
             $out = glob("$problem_dir/out/*");
             
-			if ($in){
-				//rename input and output file base on file name order
-				if ($rename_inputoutput){
-					if (count($in) != count($out)){
-                        $messages[] = 'The zip contain mismatch number of input and output files: ' . count($in) . ' input files vs ' . count($out) . ' output files';					}
-					else {
-						for($i = 1; $i <= count($in); $i++){
-							rename($in[$i-1], "$problem_dir/in/input$i.txt");
-							rename($out[$i-1], "$problem_dir/out/output$i.txt");
-						}
-					}
-				} else {
+            if ($in){
+                //rename input and output file base on file name order
+                if ($rename_inputoutput){
+                    if (count($in) != count($out)){
+                        $messages[] = 'The zip contain mismatch number of input and output files: ' . count($in) . ' input files vs ' . count($out) . ' output files';                  }
+                    else {
+                        for($i = 1; $i <= count($in); $i++){
+                            rename($in[$i-1], "$problem_dir/in/input$i.txt");
+                            rename($out[$i-1], "$problem_dir/out/output$i.txt");
+                        }
+                    }
+                } else {
                     //Check input and output file but won't rename
                     // var_dump($problem_dir);die();$problem_dir."/out/output$i.txt"
-					for($i = 0; $i < count($in); $i++){
+                    for($i = 0; $i < count($in); $i++){
                         $real_id = $i+1;
-						if (!in_array($problem_dir."/in/input$real_id.txt",$in)){
+                        if (!in_array($problem_dir."/in/input$real_id.txt",$in)){
                                                         $messages[]= "A file name input$real_id.txt seem to be missing in your folder";
-						} else {
-							if (!in_array($problem_dir."/out/output$real_id.txt",$out)){
+                        } else {
+                            if (!in_array($problem_dir."/out/output$real_id.txt",$out)){
                                 $messages[] = "A file name output$real_id.txt seem to be missing in your folder";
-							}
-						}
-					}
-				}
-			}
-		}
-		else
-		{
-			$messages[] = 'Error: Error extracting zip archive.';
-		}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            $messages[] = 'Error: Error extracting zip archive.';
+        }
 
-		// Remove temp directory
-		shell_exec("rm -rf $tmp_dir");
-	}
+        // Remove temp directory
+        shell_exec("rm -rf $tmp_dir");
+    }
     public function get_directory_path($id = NULL){
         if ($id === NULL) return NULL;
         
-		$assignments_root = Setting::get("assignments_root");
+        $assignments_root = Setting::get("assignments_root");
         
         $problem_dir = $assignments_root . "/problems/".$id;
        
@@ -387,41 +394,41 @@ class problem_controller extends Controller
     }
 
     private function clean_up_old_problem_dir($problem_dir){
-		$remove = 
-		" rm -rf $problem_dir/in $problem_dir/out $problem_dir/tester*"
-			."  $problem_dir/template.* "
-			."  $problem_dir/desc.*  $problem_dir/*.pdf; done";
-		//echo "cp -R $tmp_dir/* $problem_dir;";			
-		//echo $remove; die();			
-		shell_exec($remove); 
+        $remove = 
+        " rm -rf $problem_dir/in $problem_dir/out $problem_dir/tester*"
+            ."  $problem_dir/template.* "
+            ."  $problem_dir/desc.*  $problem_dir/*.pdf; done";
+        //echo "cp -R $tmp_dir/* $problem_dir;";            
+        //echo $remove; die();          
+        shell_exec($remove); 
 
-		mkdir("$problem_dir/in", 0700, TRUE);
-		mkdir("$problem_dir/out", 0700, TRUE);
-			
-	}
+        mkdir("$problem_dir/in", 0700, TRUE);
+        mkdir("$problem_dir/out", 0700, TRUE);
+            
+    }
 
     public function get_description($id = NULL){
         $problem_dir = $this->get_directory_path($id);
         
-		$result =  array(
-			'description' => '<p>Description not found</p>',
-			'has_pdf' => glob("$problem_dir/*.pdf") != FALSE,
-			'has_template' => glob("$problem_dir/template.cpp") != FALSE
+        $result =  array(
+            'description' => '<p>Description not found</p>',
+            'has_pdf' => glob("$problem_dir/*.pdf") != FALSE,
+            'has_template' => glob("$problem_dir/template.cpp") != FALSE
         );
-		
-		$path = "$problem_dir/desc.html";
         
-		if (file_exists($path))
+        $path = "$problem_dir/desc.html";
+        
+        if (file_exists($path))
             $result['description'] = file_get_contents($path);   
        
-		return $result;
-	}
+        return $result;
+    }
     
     public function delete_problem($id){
         
         $cmd = 'rm -rf '.$this->get_directory_path($id);
       
-		 // If you want to set transaction time, you can append the new argument in the transaction function
+         // If you want to set transaction time, you can append the new argument in the transaction function
         // dd($id);
         DB::beginTransaction();  
 
@@ -450,46 +457,38 @@ class problem_controller extends Controller
   
     /** Dowload file pdf  */
     public function pdf($problem_id)
-	{
+    {
         // Find pdf file
-		if ($problem_id === NULL)
+        if ($problem_id === NULL)
             abort(404);
         else
             $pattern = $this->get_directory_path($problem_id)."/*.pdf";
-			
+            
         $pdf_files = glob($pattern);
         $pdf_files = implode("|",$pdf_files);
         
-		if ( ! $pdf_files )
+        if ( ! $pdf_files )
             abort(404,"File not found");
 
-		// Download the file to browser
+        // Download the file to browser
         return response()->download($pdf_files);
     
     }
     
-    public function new_problem_id(){
-		$max = Problem::count()+1 ;
-		$assignments_root = Setting::get("assignments_root");
-		while (file_exists($assignments_root.'/problems/'.$max)){
-			$max++;
-        }
-		return $max;
-    }
 
     public function get_template_path($problem_id = NULL){
-		$pattern1 = rtrim($this->get_directory_path($problem_id)
-		."/template.public.cpp");
+        $pattern1 = rtrim($this->get_directory_path($problem_id)
+        ."/template.public.cpp");
 
-		$template_file = glob($pattern1);
-		if ( ! $template_file ){
-			$pattern = rtrim($this->get_directory_path($problem_id)
-						."/template.cpp");
+        $template_file = glob($pattern1);
+        if ( ! $template_file ){
+            $pattern = rtrim($this->get_directory_path($problem_id)
+                        ."/template.cpp");
 
-			$template_file = glob($pattern);
-		}
-		return $template_file;
-	}
+            $template_file = glob($pattern);
+        }
+        return $template_file;
+    }
 
     // public function test()
     // {
@@ -528,40 +527,40 @@ class problem_controller extends Controller
             $prefix_name = $name_with_extension[0];
             
             if (substr($prefix_name, 0, 5) == 'input') {
-				$in[end($name)] = $data[$i];
-			} else if (substr($prefix_name, 0, 6) == 'output'){
-				$out[end($name)] = $data[$i];
-			} else {
-				$files[end($name)] = $data[$i];
-			}
+                $in[end($name)] = $data[$i];
+            } else if (substr($prefix_name, 0, 6) == 'output'){
+                $out[end($name)] = $data[$i];
+            } else {
+                $files[end($name)] = $data[$i];
+            }
         }
         
         if (!isset($files['desc.html'])){
-			$messages[] = "Your test folder doesn't have desc.html file for problem description";
+            $messages[] = "Your test folder doesn't have desc.html file for problem description";
         }
     
         for($i = 1; $i < count($in); $i++){
-			if (!isset($in["input$i.txt"])){
+            if (!isset($in["input$i.txt"])){
                 $messages[] = "A file name input$i.txt seem to be missing in your folder";
-			} else {
-				if (!isset($out["output$i.txt"])){
+            } else {
+                if (!isset($out["output$i.txt"])){
                     $messages[] = "A file name output$i.txt seem to be missing in your folder";
-				}
-			}
+                }
+            }
         }
         
         $this->clean_up_old_problem_dir($problem_dir);
 
-		foreach($in as $name => $tmp_name ){
+        foreach($in as $name => $tmp_name ){
             rename($tmp_name, "$problem_dir/in/$name");
         }
         
-		foreach($out as $name => $tmp_name ){
-			rename($tmp_name, "$problem_dir/out/$name");
+        foreach($out as $name => $tmp_name ){
+            rename($tmp_name, "$problem_dir/out/$name");
         }
         
-		foreach($files as $name => $tmp_name ){
-			rename($tmp_name, "$problem_dir/$name");
+        foreach($files as $name => $tmp_name ){
+            rename($tmp_name, "$problem_dir/$name");
         }
     } 
     
@@ -570,7 +569,7 @@ class problem_controller extends Controller
         DB::beginTransaction(); 
 
         $time_limit = $request->time_limit;
-		$memory_limit = $request->memory_limit;
+        $memory_limit = $request->memory_limit;
         $enable = $request->enable;
         
         $problem->languages()->detach();
@@ -583,7 +582,7 @@ class problem_controller extends Controller
                             'memory_limit' => $memory_limit[$i],
                         ]
                     );
-			    }
+                }
         }
 
         DB::commit();
