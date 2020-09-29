89967a97015dc8b56b049d2fa7c967ac1fdaf71a
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2020-09-29 16:07:14 +0700

    fix for deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //Force root URL to allow fixed url when running behind proxy
+        // code getting for stack overflow https://stackoverflow.com/questions/35304448/laravel-change-base-url
+        \URL::forceRootUrl(\Config::get('app.url'));  
+        if (\Str::contains(\Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+            //use \URL:forceSchema('https') if you use laravel < 5.4
+        }
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return redirect('login');
+    return redirect(route('home'));
 });
 
 Auth::routes();
