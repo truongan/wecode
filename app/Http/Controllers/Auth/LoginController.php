fe117cee3e493c14b4ce29a96456c2761fc09560
An at home workstation <hostmaster@truongan.name.vn>
2020-10-02 01:55:30 +0700

    clean up code

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
