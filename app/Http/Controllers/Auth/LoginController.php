7e4638dac653b354c4749b41cc38d1e88d4b5735
An at home workstation <hostmaster@truongan.name.vn>
2020-10-02 02:15:40 +0700

    silli

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -65,7 +65,7 @@ class LoginController extends Controller
 
     public function logout(Request $request){
         Auth::logout();
-        return redirect()->route('login')
+        return redirect()->route('login');
     }
     
 }
