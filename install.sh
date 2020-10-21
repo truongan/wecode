15c5cce4ccb1d111bd819f5407159163daeaba26
An at home workstation <hostmaster@truongan.name.vn>
2020-10-21 19:51:57 +0700

    dump forgetful

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ fi
 if [ "$password" = "" ]; then
 	usage; exit 1
 fi
-echo $site_url ; exit 0
+echo $site_url
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
