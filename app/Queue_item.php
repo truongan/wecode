8586be897ac5322bb43c6f64b18467d40f713f44
An at home workstation <hostmaster@truongan.name.vn>
2020-10-23 03:11:25 +0700

    typo

--- app/Queue_item.php
+++ app/Queue_item.php
@@ -35,7 +35,7 @@ class Queue_item extends Model
 		$item = NULL;
 		// dd(Queue_item::whereNotNull('processid')->count());
 		if(Queue_item::whereNotNull('processid')->count() < $limit){
-			$item = Queue_item::whereNull('processid')->with('submission.problem', 'submission.user', 'submission.language')->oldest()->->assignment->total_submit()->first();
+			$item = Queue_item::whereNull('processid')->with('submission.problem', 'submission.user', 'submission.language')->oldest()->lockForUpdate()->first();
 			// dd($item);
 			if ($item != NULL){
 				$item->processid = getmypid();
