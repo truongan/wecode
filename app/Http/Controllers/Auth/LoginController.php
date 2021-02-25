52a68739618a5a4685fbcdce455a67be68188fa7
An at home workstation <hostmaster@truongan.name.vn>
2021-02-25 16:50:35 +0700

    clean up + unenrol confirmation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
@@ -106,21 +106,16 @@ class LoginController extends Controller
 				return true;
 			}
 			else {
-				///Optional: create user if not present.
-                // User::insert([
+				///Optional: create ldap user if not present.
+                // $user = User::create([
                 //     'username' => $ldap_user['masv'],
                 //     'email' => $ldap_user['email'],
                 //     'display_name' => $ldap_user['hoten'],
-                //     // 'email' => Str::random(10).'@gmail.com',
                 //     'password' => Hash::make(Str::random(80)),
                 //     'role_id' => $ldap_user['GV']?'3' : '4' // Trợ giảng hoặc sinh viên thôi.
                 // ]);
-
-				// $this->user_model->add_user(
-				// 	$ldap_user['masv'], $ldap_user['email'], shj_random_password(20)
-				// 	, $ldap_user['GV']?'head_instructor' : 'student'
-				// 	, $ldap_user['hoten']
-				// );
+                // Auth::login($user, $remember);
+                // return true;
 			}
 		}
 		return ($ldap_user && $user_id);
