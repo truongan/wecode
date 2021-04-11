68821b498887dea79bb4d061d10a8347efd2cde5
An at home workstation <hostmaster@truongan.name.vn>
2021-04-11 23:38:54 +0700

    update compiler

--- tester/compile_c.sh
+++ tester/compile_c.sh
@@ -48,7 +48,7 @@ if [ $NEEDCOMPILE -eq 0 ]; then
     EXITCODE=110
 else
     mv code.c code.$EXT
-    $tester_dir/run_judge_in_docker.sh `pwd` gcc:6 $COMPILER code.$EXT $C_OPTIONS $C_WARNING_OPTION -o $EXEFILE >/dev/null 2>cerr
+    $tester_dir/run_judge_in_docker.sh `pwd` gcc:10 $COMPILER code.$EXT $C_OPTIONS $C_WARNING_OPTION -o $EXEFILE >/dev/null 2>cerr
     EXITCODE=$?
 
 fi
