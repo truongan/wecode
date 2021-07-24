672c597349681715bec3e082c00b17b3b110c568
An at home workstation <hostmaster@truongan.name.vn>
2021-07-25 02:08:44 +0700

    cleanup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,13 +37,10 @@
         </div>
     
     </div>
-    <script type="text/javascript" src="{{ asset('js/jquery-3.4.1.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/popper.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-
+    <script type="text/javascript" src="{{ asset('assets/jquery-3.6.0.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 
     {{-- <script type="text/javascript" src="{{ asset('assets/js/moment.min.js') }}"></script> --}}
-    <script type="text/javascript" src="{{ asset('assets/js/jquery.cookie.js') }}"></script>
     <script type='text/javascript' src="{{ asset('assets/sbadmin/js/sb-admin.min.js') }}"></script>
 
     <script	src="{{ asset('assets/js/notify.min.js') }}"></script>
