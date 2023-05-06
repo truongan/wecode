bbbc59eca0806f059d5ac36bd02257d5506b0d88
ntkhang2003 <ntkhang2003@gmail.com>
2023-05-06 15:45:29 +0700

    change datatype of  to longtext

--- database/migrations/2023_05_06_121728_add_data_column_to_scoreboards.php
+++ database/migrations/2023_05_06_121728_add_data_column_to_scoreboards.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('scoreboards', function (Blueprint $table) {
-            $table->text('data')->nullable();
+            $table->longtext('data')->nullable();
         });
     }
 
