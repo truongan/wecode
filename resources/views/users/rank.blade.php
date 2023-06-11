0b075eb4caf26183a17c45692c2fc595269ebaa9
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2023-06-11 10:12:29 +0700

    host datatable from server, NO CDN anymore

--- resources/views/users/rank.blade.php
+++ resources/views/users/rank.blade.php
@@ -7,7 +7,7 @@
 Users - ranking
 @endsection
 @section('other_assets')
-  <link rel='stylesheet' type='text/css' href='https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css'/>
+  <link rel='stylesheet' type='text/css' href='{{ asset('assets/DataTables/datatables.min.css') }}'/>
 @endsection
 @section('content')
 
@@ -66,7 +66,7 @@ Users - ranking
 @section('body_end')
 
 
-<script type="text/javascript" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/DataTables/datatables.min.css') }}"></script>
 
 <script>
 
