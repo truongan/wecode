091ee938ea7e91c464ebc95ec0bd14c8e55ae9bf
An at home workstation <hostmaster@truongan.name.vn>
2025-07-30 02:38:37 +0700

    update to database for new coefficient late rule

---
+++ database/migrations/2025_07_28_190554_switch_to_experession_language.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Setting;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            //
+            $table->double('coefficient')->change();
+        });
+        Setting::set('default_late_rule', 'delay < 0 ? 100 : 100*pow(0.99, delay/3600');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            //
+            $table->string('coefficient')->change();
+        });
+    }
+};
