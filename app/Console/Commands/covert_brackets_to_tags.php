4d5ace99533de235676910c4e11fd3716de1f57f
truongan@my.macbook.air <truongan@my.macbook.air>
2025-11-11 05:02:37 +0700

    convert bracket to tags

---
+++ app/Console/Commands/covert_brackets_to_tags.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Problem;
+use App\Models\Tag;
+
+class covert_brackets_to_tags extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'app:covert_brackets_to_tags';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description';
+
+	/**
+	 * Execute the console command.
+	 */
+	public function handle()
+	{
+		$all_prob = Problem::pluck('name', 'id');
+
+		foreach ($all_prob as $id => $name){
+			$count = preg_match_all('/\[(.*?)\]/', $name, $matches);
+			if( $count == 0 ) continue;
+            $prob = Problem::find($id);
+
+
+			$new_name = preg_replace('/\[(.*?)\]/', '', $name);
+            echo ("Problem {$id} got the following brackets: " . print_r($matches[1], true) . "rename to {$new_name}\n\n");
+
+            $tags = [];
+            foreach ($matches[1] as $tag_name){
+                $tags[] = Tag::firstOrCreate([
+                    'text' => $tag_name
+                ]);
+
+            }
+            $prob->tags()->syncWithoutDetaching($tags);
+            $prob->name = $new_name;
+            $prob->save();
+		}
+		// process squre bracket (the most common one)
+	}
+}
