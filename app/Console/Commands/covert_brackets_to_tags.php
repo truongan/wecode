f6a5374e87a4bf4ca9c58b57fa17cd05b405423f
truongan@my.macbook.air <truongan@my.macbook.air>
2025-11-12 14:15:45 +0700

    working on light/dark toggle

--- app/Console/Commands/covert_brackets_to_tags.php
+++ app/Console/Commands/covert_brackets_to_tags.php
@@ -13,7 +13,7 @@ class covert_brackets_to_tags extends Command
 	 *
 	 * @var string
 	 */
-	protected $signature = 'app:covert_brackets_to_tags';
+	protected $signature = 'app:covert_brackets_to_tags {--d|dry_run}';
 
 	/**
 	 * The console command description.
@@ -28,26 +28,28 @@ class covert_brackets_to_tags extends Command
 	public function handle()
 	{
 		$all_prob = Problem::pluck('name', 'id');
-
+        echo "SHIT";
 		foreach ($all_prob as $id => $name){
 			$count = preg_match_all('/\[(.*?)\]/', $name, $matches);
-			if( $count == 0 ) continue;
-            $prob = Problem::find($id);
+			// if( $count == 0 ) continue;
+			$prob = Problem::find($id);
 
 
 			$new_name = preg_replace('/\[(.*?)\]/', '', $name);
-            echo ("Problem {$id} got the following brackets: " . print_r($matches[1], true) . "rename to {$new_name}\n\n");
+            $matches[1][] = $prob->user->username; //Add uploader name as a tags
+			echo ("Problem {$id} got the following brackets: " . print_r($matches[1], true) . "rename to {$new_name}\n\n");
 
-            $tags = [];
-            foreach ($matches[1] as $tag_name){
-                $tags[] = Tag::firstOrCreate([
-                    'text' => $tag_name
-                ]);
+			if ($this->option('dry_run')) continue;
 
-            }
-            $prob->tags()->syncWithoutDetaching($tags);
-            $prob->name = $new_name;
-            $prob->save();
+			$tags = [];
+			foreach ($matches[1] as $tag_name){
+				$tags[] = Tag::firstOrCreate([
+					'text' => $tag_name
+				]);
+			}
+			$prob->tags()->syncWithoutDetaching($tags);
+			$prob->name = $new_name;
+			$prob->save();
 		}
 		// process squre bracket (the most common one)
 	}
